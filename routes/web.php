a76222ad016c2eb8fb2c7864af1cac8e4d3ef139
syuzrh27 <syuja27@gmail.com>
2023-08-30 12:26:27 +0700

    ubah web.php

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,15 @@ Route::get('/about', function () {
         'position3'=>'Chief Developer',
         'company'=>'Rexi'
     ]);
-});
+});?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
